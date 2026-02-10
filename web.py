1522a2041830300dae2a667c31362e06ebc368b5
brechtvdv <br5cht@hotmail.com>
2026-02-10 09:29:50 +0100

    Retrieve tasks from delta before processing

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 import os
 
+from src.sparql_config import JOB_STATUSES
 from src.task import Task
 
 from fastapi import APIRouter, BackgroundTasks
@@ -33,8 +34,31 @@ class NotificationResponse(BaseModel):
 @router.post("/delta", status_code=202)
 async def delta(data: list[DeltaNotification], background_tasks: BackgroundTasks) -> NotificationResponse:
     print("Received delta notification with", data, "patches")
-    for patch in data:
-        for ins in patch.inserts:
-            task = Task.from_uri(ins.subject.value)
-            background_tasks.add_task(task.execute)
-    return NotificationResponse(status="accepted", message="Processing started")
+    inserts = [
+        ins
+        for patch in data
+        for ins in patch.inserts
+    ]
+
+    scheduled_tasks = [
+        ins.subject.value
+        for ins in inserts
+        if ins.predicate.value == "http://www.w3.org/ns/adms#status"
+        and ins.object.value == JOB_STATUSES["scheduled"]
+    ]
+    print("Scheduled tasks in delta:", scheduled_tasks)
+    if not scheduled_tasks:
+        print("delta did not contain scheduled tasks")
+        return NotificationResponse(
+            status="ignored",
+            message="delta didn't contain download jobs, ignoring",
+        )
+
+    for uri in scheduled_tasks:
+        task = Task.from_uri(uri)
+        background_tasks.add_task(task.execute)
+
+    return NotificationResponse(
+        status="accepted",
+        message="Processing started",
+    )
