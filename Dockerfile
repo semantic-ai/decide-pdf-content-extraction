9cff8a98847b4e4c69aec19e2a402a618ee011df
karel kremer <karel.kremer@redpencil.io>
2026-02-12 11:40:38 +0100

    use fastapi feature branch and wait for triplestore

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM local-python-template
+FROM semtech/mu-python-template:feature-fastapi
 #FROM brechtvdv/mu-python-template:feature-fastapi-arm # for arm64 architecture (Apple M1/M2)
 LABEL maintainer="ward@ml2grow.com"
 
--- web.py
+++ web.py
@@ -1,10 +1,31 @@
 from src.job import fail_busy_and_scheduled_tasks
 from src.sparql_config import get_prefixes_for_query, GRAPHS, JOB_STATUSES, TASK_OPERATIONS, prefixed_log
 from src.task import Task
-from helpers import query, wait_for_triplestore
+from helpers import query, log
+import time
 
 from fastapi import APIRouter, BackgroundTasks
 from pydantic import BaseModel
+
+def wait_for_triplestore():
+    triplestore_live = False
+    log("Waiting for triplestore...")
+    while not triplestore_live:
+        try:
+            result = query(
+                """
+                SELECT ?s WHERE {
+                ?s ?p ?o.
+                } LIMIT 1""",
+            )
+            if result["results"]["bindings"][0]["s"]["value"]:
+                triplestore_live = True
+            else:
+                raise Exception("triplestore not ready yet...")
+        except Exception as _e:
+            log("Triplestore not live yet, retrying...")
+            time.sleep(1)
+    log("Triplestore ready!")
 
 
 @app.on_event("startup")
@@ -34,6 +55,13 @@ def delta(background_tasks: BackgroundTasks) -> NotificationResponse:
         message="Processing started",
     )
 
+@router.get("/test", status_code=202)
+def testendpoint():
+    res = query("SELECT * WHERE { ?s ?p ?o} LIMIT 1", sudo=True)
+    return {
+        "success": True,
+        "res": res
+    }
 
 def process_open_tasks():
     prefixed_log("Checking for open tasks...")
